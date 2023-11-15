9a289f7bfbc2faa0b3ab28e2b6ec64d6b14d6a16
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-11-15 15:29:24 -0300

    #	modified:   package.json #	modified:   prisma/schema.prisma #	modified:   src/app.module.ts #	new file:   src/application/application.controller.spec.ts #	new file:   src/application/application.controller.ts #	new file:   src/application/application.module.ts #	new file:   src/application/application.service.spec.ts #	new file:   src/application/application.service.ts #	new file:   src/application/dto/create-application.dto.ts #	new file:   src/application/dto/update-application.dto.ts #	new file:   src/application/entities/application.entity.ts #	new file:   src/group/dto/create-group.dto.ts #	new file:   src/group/dto/update-group.dto.ts #	new file:   src/group/entities/group.entity.ts #	new file:   src/group/group.controller.spec.ts #	new file:   src/group/group.controller.ts #	new file:   src/group/group.module.ts #	new file:   src/group/group.service.spec.ts #	new file:   src/group/group.service.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ApplicationModule } from './application/application.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    ApplicationModule,
+    GroupModule,
+  ],
 })
 export class AppModule {}
